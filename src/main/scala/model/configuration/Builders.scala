5220453ecac80108b54c7d90deb1fa1c8daf4733
Nicolò Malucelli <nicolo.malucelli@studio.unibo.it>
2023-06-21 16:07:25 +0200

    region builders name field private

--- src/main/scala/model/configuration/Builders.scala
+++ src/main/scala/model/configuration/Builders.scala
@@ -1,5 +1,21 @@
 package model.configuration
 
+import model.world.Region
+import model.world.BasicRegion
+
 object Builders {
+
+  trait RegionBuilder():
+    def setName(name: String): RegionBuilder
+    def build(): Option[Region]
+
+  class SimpleRegionBuilder() extends RegionBuilder:
+    private var name: Option[String] = None
+
+    override def setName(name: String): RegionBuilder = {this.name = Some(name); this}
+
+    override def build(): Option[Region] = name match
+      case name if name.isEmpty => None
+      case _ => Some(new BasicRegion(name.get, 0, 0, 0, 0, 0, 0, 0))
 
 }
--- src/test/scala/model/configuration/RegionBuilderTest.scala
+++ src/test/scala/model/configuration/RegionBuilderTest.scala
@@ -1,5 +1,17 @@
 package model.configuration
 
+
+import model.configuration.Builders.{RegionBuilder, SimpleRegionBuilder}
+import org.junit.Assert.assertEquals
+import org.junit.{Before, Test}
+
 class RegionBuilderTest {
 
+  var regionBuilder: RegionBuilder = new SimpleRegionBuilder()
+
+  @Test
+  def testCannotBuildWithoutSettingMandatoryFields(): Unit =
+    assertEquals(None, regionBuilder.build())
+
 }
+
