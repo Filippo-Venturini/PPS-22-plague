0e7f708e1c66ea8a73a16fb4364579d00d46d6a2
Nicolò Malucelli <nicolo.malucelli@studio.unibo.it>
2023-06-22 10:58:46 +0200

    adapted to new region constructor

--- src/main/scala/model/configuration/Builders.scala
+++ src/main/scala/model/configuration/Builders.scala
@@ -1,21 +1,23 @@
 package model.configuration
 
+import model.world.{BasicRegion, Name, Region, RegionConfiguration}
+
 /*import model.world.Region
 import model.world.BasicRegion*/
 
 object Builders {
 
   trait RegionBuilder():
-    def setName(name: String): RegionBuilder
-    //def build(): Option[Region]
+    def setName(name: Name): RegionBuilder
+    def build(): Option[Region]
 
   class SimpleRegionBuilder() extends RegionBuilder:
-    private var name: Option[String] = None
+    private var name: Option[Name] = None
 
     override def setName(name: String): RegionBuilder = {this.name = Some(name); this}
 
-    /*override def build(): Option[Region] = name match
+    override def build(): Option[Region] = name match
       case name if name.isEmpty => None
-      case _ => Some(new BasicRegion(name.get, 0, 0, 0, 0, 0, 0, 0))
-*/
+      case _ => Some(new BasicRegion(RegionConfiguration(name.get, 0, 0, 0, 0, 0, 0)))
+
 }
--- src/test/scala/model/configuration/RegionBuilderTest.scala
+++ src/test/scala/model/configuration/RegionBuilderTest.scala
@@ -2,16 +2,20 @@ package model.configuration
 
 
 import model.configuration.Builders.{RegionBuilder, SimpleRegionBuilder}
+import model.world.RegionConfiguration
 import org.junit.Assert.assertEquals
 import org.junit.{Before, Test}
 
 class RegionBuilderTest {
 
+  private val regionConfiguration = RegionConfiguration("Europe", 60_000_000, 0, 0 ,0, 0, 0)
   var regionBuilder: RegionBuilder = new SimpleRegionBuilder()
 
-  /*@Test
+  @Test
   def testCannotBuildWithoutSettingMandatoryFields(): Unit =
-    assertEquals(None, regionBuilder.build())*/
+    assertEquals(None, regionBuilder.build())
+
+
 
 }
 
