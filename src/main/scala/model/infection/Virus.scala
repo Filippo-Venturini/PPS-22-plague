5860eae81b527e6435a5d38a8a7b618308e64c2c
Alberto Di Girolamo <alberto.digirolamo2@studio.unibo.it>
2023-06-22 14:56:45 +0200

    enable test for virus and infection

--- src/main/scala/model/infection/Virus.scala
+++ src/main/scala/model/infection/Virus.scala
@@ -1,24 +1,40 @@
 package model.infection
+import model.world.RegionConfiguration
 
-trait VirusStructure:
-  type Name = String
-  type ColdRegionsInfectivity = Int
-  type WarmRegionsInfectivity = Int
-  type LowDensityRegionInfectivity = Int
-  type HighDensityRegionsInfectivity = Int
-  type RichRegionsInfectivity = Int
-  type PoorRegionsInfectivity = Int
-  type VaccineResistance = Int
-  type AirportEnabled = Boolean
-  type PortEnabled = Boolean
+type Name = String
+type ColdRegionsInfectivity = Int
+type WarmRegionsInfectivity = Int
+type LowDensityRegionInfectivity = Int
+type HighDensityRegionsInfectivity = Int
+type RichRegionsInfectivity = Int
+type PoorRegionsInfectivity = Int
+type VaccineResistance = Int
+type AirportEnabled = Boolean
+type PortEnabled = Boolean
 
-  case class Virus(name: Name,
-                   coldRegionsInfectivity: ColdRegionsInfectivity,
-                   warmRegionsInfectivity: WarmRegionsInfectivity,
-                   LowDensityRegionInfectivity: LowDensityRegionInfectivity,
-                   highDensityRegionsInfectivity: HighDensityRegionsInfectivity,
-                   richRegionsInfectivity: RichRegionsInfectivity,
-                   poorRegionsInfectivity: PoorRegionsInfectivity,
-                   vaccineResistance: VaccineResistance,
-                   airportEnabled: AirportEnabled,
-                   portEnabled: PortEnabled) extends VirusStructure
+case class VirusConfiguration(name: Name,
+                              coldRegionsInfectivity: ColdRegionsInfectivity,
+                              warmRegionsInfectivity: WarmRegionsInfectivity,
+                              lowDensityRegionInfectivity: LowDensityRegionInfectivity,
+                              highDensityRegionsInfectivity: HighDensityRegionsInfectivity,
+                              richRegionsInfectivity: RichRegionsInfectivity,
+                              poorRegionsInfectivity: PoorRegionsInfectivity,
+                              vaccineResistance: VaccineResistance,
+                              airportEnabled: AirportEnabled,
+                              portEnabled: PortEnabled)
+
+abstract class Virus:
+  def virusConfiguration: VirusConfiguration
+
+  val name: Name = virusConfiguration.name
+  val coldRegionsInfectivity: ColdRegionsInfectivity = virusConfiguration.coldRegionsInfectivity
+  val warmRegionsInfectivity: WarmRegionsInfectivity = virusConfiguration.warmRegionsInfectivity
+  val lowDensityRegionInfectivity: LowDensityRegionInfectivity = virusConfiguration.lowDensityRegionInfectivity
+  val highDensityRegionsInfectivity: HighDensityRegionsInfectivity = virusConfiguration.highDensityRegionsInfectivity
+  val richRegionsInfectivity: RichRegionsInfectivity = virusConfiguration.richRegionsInfectivity
+  val poorRegionsInfectivity: PoorRegionsInfectivity = virusConfiguration.poorRegionsInfectivity
+  val vaccineResistance: VaccineResistance = virusConfiguration.vaccineResistance
+  val airportEnabled: AirportEnabled = virusConfiguration.airportEnabled
+  val portEnabled: PortEnabled = virusConfiguration.portEnabled
+
+class BasicVirus(override val virusConfiguration: VirusConfiguration) extends Virus
--- src/test/scala/model/infection/TestInfection.scala
+++ src/test/scala/model/infection/TestInfection.scala
@@ -7,17 +7,22 @@ import org.junit.{Before, Test}
 class TestInfection {
 
   val testRegionConfiguration: RegionConfiguration = RegionConfiguration("Europe", 746000000, 9, 5, 8, 9, 8)
-
   var testRegion: Region = new BasicRegion(testRegionConfiguration)
 
-  //var infectionLogic: InfectionLogic = new InternalInfectionLogic(testRegion, new Virus("DHT11"))
+  val testVirusConfiguration: VirusConfiguration = VirusConfiguration("DHT11", 0, 0, 0, 0, 0, 0, 0, false, false)
+  val virus: Virus = new BasicVirus(testVirusConfiguration)
+  var infectionLogic: InfectionLogic = new InternalInfectionLogic(testRegion, virus)
 
-  /*
   @Test
   def testInternalIncrementInfection: Unit =
     testRegion.infectedAmount = 1
-    //infectionLogic.compute()
+    infectionLogic.compute()
     assertEquals(testRegion.infectedAmount, 2)
 
+  /*
+  @Test
+  def testExternalIncrementInfection: Unit =
+
   */
+
 }
