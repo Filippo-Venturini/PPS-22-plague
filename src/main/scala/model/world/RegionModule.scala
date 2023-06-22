44a3c58c9a4fa5b26f5dc215d71cc37d46a396cd
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-06-22 15:28:32 +0200

    Add types import

--- src/main/scala/model/world/RegionModule.scala
+++ src/main/scala/model/world/RegionModule.scala
@@ -17,35 +17,35 @@ abstract class Region:
   protected var borderingRegions: List[Region] = List()
   def infectedAmount: Int
   def infectedAmount_= (newAmount: Int): Unit
-  
+
   /**
-   * 
+   *
    * @param borderingRegion a region that is directly connected by border
    */
   def addBorderingRegion(borderingRegion: Region): Unit = this.borderingRegions = this.borderingRegions :+ borderingRegion
 
   /**
-   * 
+   *
    * @param borderingRegions a List of regions that will be add to the bordering regions
    */
   def addBorderingRegions(borderingRegions: List[Region]): Unit = this.borderingRegions = this.borderingRegions ::: borderingRegions
 
   /**
-   * 
+   *
    * @return the list of all the region reachable
    */
   def getReachableRegions: List[ReachableRegion]
 
 /**
  * Class that represent a basic region without ports or airports
- * 
+ *
  * @param regionConfiguration the configuration that contains all the region's characteristics
  */
 class BasicRegion (override val regionConfiguration: RegionConfiguration) extends Region:
   override var infectedAmount: Int = 0
 
   /**
-   * 
+   *
    *  @return the list of all the region reachable, so only the bordering ones
    */
   override def getReachableRegions: List[ReachableRegion] = borderingRegions.map(r => (r, ReachableMode.Border))
@@ -57,14 +57,14 @@ trait WithPort extends Region:
   def portRoutes: List[Route] = List()
 
   /**
-   * 
+   *
    *  @return the list of all the region reachable, so the bordering and also the reachable with port
    */
   abstract override def getReachableRegions: List[ReachableRegion] = super.getReachableRegions
 
 /**
  * Class that represent a region with a port
- * 
+ *
  * @param regionConfiguration the configuration that contains all the region's characteristics
  */
 class RegionWithPort(regionConfiguration: RegionConfiguration) extends BasicRegion(regionConfiguration) with WithPort
