919e2d3c7e5a90e864a1719033260f669e2334ac
Alberto Di Girolamo <alberto.digirolamo2@studio.unibo.it>
2023-06-23 13:52:47 +0200

    Fix Infection Handler structure

--- src/test/scala/model/infection/TestInfection.scala
+++ src/test/scala/model/infection/TestInfection.scala
@@ -10,19 +10,22 @@ class TestInfection {
   val testVirusConfiguration: VirusConfiguration = VirusConfiguration("DHT11", 0, 0, 0, 0, 0, 0, 0, false, false)
   val virus: Virus = new BasicVirus(testVirusConfiguration)
 
-
   @Test
   def testInternalIncrementInfection: Unit =
     val testRegionConfiguration: RegionConfiguration = RegionConfiguration("Europe", 746000000, 9, 5, 8, 9, 8)
     val testRegion: Region = new BasicRegion(testRegionConfiguration)
 
+    testRegion.infectedAmount = 1
+
+    val regions: List[Region] = List(testRegion)
+    val infectionHandler: InfectionHandler = new InfectionHandler(virus, regions)
+
     val infectionLogic: InfectionLogic = new InternalInfectionLogic(testRegion, virus)
 
-    testRegion.infectedAmount = 1
-    infectionLogic.compute()
-    assertEquals(2, testRegion.infectedAmount)
 
-  /*
+    infectionHandler.computeInfection(regions)(using infectionLogic)
+    assert(testRegion.infectedAmount > 1)
+
   @Test
   def testExternalIncrementInfection: Unit =
     val testInfectedRegionConfiguration: RegionConfiguration = RegionConfiguration("Europe", 746000000, 9, 5, 8, 9, 8)
@@ -33,11 +36,15 @@ class TestInfection {
     val testSaneRegionConfiguration: RegionConfiguration = RegionConfiguration("NorthAfrica", 243000000, 2, 9, borderControl, 2, 2)
     val testSaneRegion: Region = new BasicRegion(testInfectedRegionConfiguration)
 
+    val regions: List[Region] = List(testInfectedRegion, testSaneRegion)
+    val infectionHandler: InfectionHandler = new InfectionHandler(virus, regions)
+
     val infectionLogic: InfectionLogic = new ExternalInfectionLogic(testSaneRegion, virus)
+
     testSaneRegion.addBorderingRegion(testInfectedRegion)
 
-    infectionLogic.compute()
-    assert(testSaneRegion.infectedAmount > 0)
-  */
+    infectionHandler.computeInfection(regions)(using infectionLogic)
 
+    assert(testSaneRegion.infectedAmount > 0)
+  
 }
