d49f6206747850f3a8fff1efeac30f5dd3bfbb25
Nicolò Malucelli <nicolo.malucelli@studio.unibo.it>
2023-06-23 18:53:39 +0200

    test RegionReader bad cases

--- src/main/scala/model/configuration/Readers.scala
+++ src/main/scala/model/configuration/Readers.scala
@@ -7,7 +7,7 @@ import model.world.RegionTypes.{BordersControl, Climate, Globalization, Populati
 object Readers:
 
   trait Reader
-  
+
   object Region:
     private enum RegionConfigurationFileFormat(val castCondition: String => Boolean, val setter: (RegionBuilder, String) => RegionBuilder):
       case NAME extends RegionConfigurationFileFormat(_ => true, (b, s) => b.setName(s))
@@ -17,17 +17,17 @@ object Readers:
       case RICHNESS extends RegionConfigurationFileFormat(_.toIntOption.isDefined, (b, s) => b.setRichness(s.toInt))
       case BORDERS_CONTROL extends RegionConfigurationFileFormat(_.toIntOption.isDefined, (b, s) => b.setBordersControl(s.toInt))
       case GLOBALIZATION extends RegionConfigurationFileFormat(_.toIntOption.isDefined, (b, s) => b.setGlobalization(s.toInt))
-  
+
     trait RegionReader extends Reader :
       def read(line: String): Option[Region]
-  
+
     object RegionReader:
       def apply(): RegionReader = new SimpleRegionReader
       private class SimpleRegionReader extends RegionReader:
         def read(line: String): Option[Region] =
           val params = line.split(",").toList
           RegionConfigurationFileFormat.values
-            .foldLeft(RegionBuilder())((builder, field) => field.castCondition.apply(params(field.ordinal)) && field.ordinal < line.size match
+            .foldLeft(RegionBuilder())((builder, field) => field.ordinal < params.size && field.castCondition.apply(params(field.ordinal)) match
               case true => field.setter.apply(builder, params(field.ordinal))
               case false => builder)
             .build()
--- src/test/scala/model/configuration/TestReaders.scala
+++ src/test/scala/model/configuration/TestReaders.scala
@@ -10,20 +10,39 @@ object TestReaders {
 
   class TestRegionReader {
     
-      val reader: RegionReader = RegionReader()
+    val reader: RegionReader = RegionReader()
 
 
     @Test
-      def testCorrectRowRead(): Unit =
-        val conf = RegionConfiguration("Central-Europe", 60_000_000, 6, 6, 8, 9, 10)
-        val region: Region = reader.read(s"${conf.name},${conf.population},${conf.populationDensity},${conf.climate},${conf.richness},${conf.bordersControl},${conf.globalization}").get
-        assertEquals(conf.name, region.name)
-        assertEquals(conf.population, region.population)
-        assertEquals(conf.populationDensity, region.populationDensity)
-        assertEquals(conf.climate, region.climate)
-        assertEquals(conf.richness, region.richness)
-        assertEquals(conf.bordersControl, region.bordersControl)
-        assertEquals(conf.globalization, region.globalization)
+    def testCorrectRowRead(): Unit =
+      val conf = RegionConfiguration("Central-Europe", 60_000_000, 6, 6, 8, 9, 10)
+      val region: Region = reader.read(s"${conf.name},${conf.population},${conf.populationDensity},${conf.climate},${conf.richness},${conf.bordersControl},${conf.globalization}").get
+      assertEquals(conf.name, region.name)
+      assertEquals(conf.population, region.population)
+      assertEquals(conf.populationDensity, region.populationDensity)
+      assertEquals(conf.climate, region.climate)
+      assertEquals(conf.richness, region.richness)
+      assertEquals(conf.bordersControl, region.bordersControl)
+      assertEquals(conf.globalization, region.globalization)
 
+    @Test
+    def testBadComposedString(): Unit =
+      assertEquals(None, reader.read("Central-Europe,foo,foo,foo,0,foo,0"))
+
+    @Test
+    def testUncompletedString(): Unit =
+      assertEquals(None, reader.read("Central-Europe,60000000"))
+
+    @Test
+    def testReadIgnoresAdditionalFields(): Unit =
+      val conf = RegionConfiguration("Central-Europe", 60_000_000, 6, 6, 8, 9, 10)
+      val region: Region = reader.read(s"${conf.name},${conf.population},${conf.populationDensity},${conf.climate},${conf.richness},${conf.bordersControl},${conf.globalization},a,b,c,d,e").get
+      assertEquals(conf.name, region.name)
+      assertEquals(conf.population, region.population)
+      assertEquals(conf.populationDensity, region.populationDensity)
+      assertEquals(conf.climate, region.climate)
+      assertEquals(conf.richness, region.richness)
+      assertEquals(conf.bordersControl, region.bordersControl)
+      assertEquals(conf.globalization, region.globalization)
   }
 }
