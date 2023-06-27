4452195d4b591612ac825c26bddd9f78f35ccb78
Nicolò Malucelli <nicolo.malucelli@studio.unibo.it>
2023-06-27 14:15:12 +0200

    add event click to world mapimage

--- src/main/scala/view/WorldMapPanel.scala
+++ src/main/scala/view/WorldMapPanel.scala
@@ -1,12 +1,28 @@
 package view
 
-import java.awt.{Graphics, Image}
+import java.awt.event.{MouseEvent, MouseListener}
+import java.awt.image.BufferedImage
+import java.awt.{Graphics, image}
 import javax.imageio.ImageIO
-import javax.swing.{ImageIcon, JPanel}
+import javax.swing.{ImageIcon, JButton, JPanel}
+
+type RGBCode = (Int, Int, Int)
 
 class WorldMapPanel extends JPanel:
-  val mapImage: Image = ImageIO.read(getClass().getResource("/world.png")).getScaledInstance(1208,600, Image.SCALE_DEFAULT)
+  val mapImage: BufferedImage = ImageIO.read(getClass().getResource("/map.png"))
+  this.addMouseListener(new MapMouseListener(mapImage))
 
   override def paintComponent(g: Graphics): Unit =
     super.paintComponent(g)
     g.drawImage(mapImage, 0, 0, null)
+
+class MapMouseListener(val image: BufferedImage) extends MouseListener:
+  override def mouseClicked(e: MouseEvent): Unit = println(image.getRGBCode(e.getX, e.getY));
+  override def mousePressed(e: MouseEvent): Unit = {}
+  override def mouseExited(e: MouseEvent): Unit = {}
+  override def mouseReleased(e: MouseEvent): Unit = {}
+  override def mouseEntered(e: MouseEvent): Unit = {}
+extension(image: BufferedImage)
+  def getRGBCode(x: Int, y: Int): RGBCode =
+    val rgbValue: Int = image.getRGB(x, y)
+    ((rgbValue & 0x00ff0000) >> 16, (rgbValue & 0x0000ff00) >> 8, rgbValue & 0x000000ff)
