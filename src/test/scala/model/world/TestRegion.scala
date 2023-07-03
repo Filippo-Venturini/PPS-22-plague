b08fde8e362d688f9c0e8cc54f8cc985817e0d80
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-07-03 14:18:38 +0200

    Refactor regions tests

--- src/test/scala/model/world/TestRegion.scala
+++ src/test/scala/model/world/TestRegion.scala
@@ -1,18 +1,12 @@
 package model.world
 import model.world.RegionTypes.*
+import model.world.TestRegionConfigurations.*
 import org.junit.Assert.{assertEquals, assertFalse}
 import org.junit.{Before, Test}
 
 class TestRegion {
   val regionInfectedAmount: Int = 0
   val newInfectedAmount: Int = 10
-  val europeConfiguration: RegionConfiguration = RegionConfiguration("Europe", 746000000, 9, 5, 8, 9, 8)
-  val unitedStatesConfiguration: RegionConfiguration = RegionConfiguration("United States", 746000000, 9, 5, 8, 9, 8)
-  val russiaConfiguration: RegionConfiguration = RegionConfiguration("Russia", 143000000, 2, 3, 6, 9, 5)
-  val japanConfiguration: RegionConfiguration = RegionConfiguration("Japan", 125000000, 4, 1, 7, 4, 8)
-  val australiaConfiguration: RegionConfiguration = RegionConfiguration("Australia", 25000000, 6, 2, 1, 3, 5)
-  val northAfricaConfiguration: RegionConfiguration = RegionConfiguration("North Africa", 178000000, 3, 7, 3, 9, 2)
-  val southAfricaConfiguration: RegionConfiguration = RegionConfiguration("South Africa", 59000000, 7, 2, 5, 5, 7)
   var europe: Region = new BasicRegion(europeConfiguration)
   var russia: Region = new BasicRegion(russiaConfiguration)
   var portRouteManager: PortRouteManager = new PortRouteManager()
---
+++ src/test/scala/model/world/TestRegionConfigurations.scala
@@ -0,0 +1,12 @@
+package model.world
+
+import model.world.RegionTypes.RegionConfiguration
+
+object TestRegionConfigurations :
+  val europeConfiguration: RegionConfiguration = RegionConfiguration("Europe", 746000000, 9, 5, 8, 9, 8)
+  val unitedStatesConfiguration: RegionConfiguration = RegionConfiguration("United States", 746000000, 9, 5, 8, 9, 8)
+  val russiaConfiguration: RegionConfiguration = RegionConfiguration("Russia", 143000000, 2, 3, 6, 9, 5)
+  val japanConfiguration: RegionConfiguration = RegionConfiguration("Japan", 125000000, 4, 1, 7, 4, 8)
+  val australiaConfiguration: RegionConfiguration = RegionConfiguration("Australia", 25000000, 6, 2, 1, 3, 5)
+  val northAfricaConfiguration: RegionConfiguration = RegionConfiguration("North Africa", 178000000, 3, 7, 3, 9, 2)
+  val southAfricaConfiguration: RegionConfiguration = RegionConfiguration("South Africa", 59000000, 7, 2, 5, 5, 7)
