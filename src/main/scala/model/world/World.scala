6549c52c6cab441812752aef393ba77d0a9c7850
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-07-03 11:34:41 +0200

    Add infected filters

--- src/main/scala/model/world/World.scala
+++ src/main/scala/model/world/World.scala
@@ -1,12 +1,13 @@
 package model.world
 
-import model.world.Filters.RegionFilter
+import model.world.Filters.{RegionFilter, infectedRegions}
 
 object Filters:
   type RegionFilter = Region => Boolean
 
   given RegionFilter = _ => true
   val infectedRegions: RegionFilter = region => region.infectedAmount > 0
+  val notInfectedRegions: RegionFilter = region => region.infectedAmount == 0
   val totallyInfectedRegions: RegionFilter = region => region.infectedAmount == region.population
 
 class World (private val regions: List[Region]):
--- src/test/scala/model/world/TestWorld.scala
+++ src/test/scala/model/world/TestWorld.scala
@@ -1,17 +1,35 @@
 package model.world
 
 import model.world.RegionTypes.RegionConfiguration
-import model.world.Filters.given
-import org.junit.Test
+import model.world.Filters.{infectedRegions, notInfectedRegions, totallyInfectedRegions, given}
+import org.junit.{Before, Test}
 import org.junit.Assert.assertEquals
 
 class TestWorld {
   val europeConfiguration: RegionConfiguration = RegionConfiguration("Europe", 746000000, 9, 5, 8, 9, 8)
   val unitedStatesConfiguration: RegionConfiguration = RegionConfiguration("United States", 746000000, 9, 5, 8, 9, 8)
-  var europe: Region = new BasicRegion(europeConfiguration)
-  val world: World = new World(List(europe))
+  var notInfectedRegion: Region = new BasicRegion(europeConfiguration)
+  var infectedRegion: Region = new BasicRegion(europeConfiguration)
+  val world: World = new World(List(notInfectedRegion, infectedRegion))
+
+  @Before
+  def init(): Unit =
+    infectedRegion.infectedAmount = 100
+  
+  @Test
+  def testGetAllRegions(): Unit =
+    assertEquals(List(notInfectedRegion, infectedRegion), world.getRegions)
 
   @Test
-  def testGetAllRegionsFilter(): Unit =
-    assertEquals(List(europe), world.getRegions)
+  def testGetInfectedRegions(): Unit =
+    assertEquals(List(infectedRegion), world.getRegions(using infectedRegions))
+
+  @Test
+  def testGetTotallyInfectedRegions(): Unit =
+    infectedRegion.infectedAmount = infectedRegion.population
+    assertEquals(List(infectedRegion), world.getRegions(using totallyInfectedRegions))
+
+  @Test
+  def testGetNotInfectedRegions(): Unit =
+    assertEquals(List(notInfectedRegion), world.getRegions(using notInfectedRegions))
 }
