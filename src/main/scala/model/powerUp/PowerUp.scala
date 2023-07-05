59896a336b2a197eee66f20c865b1ea71856d088
Alberto Di Girolamo <alberto.digirolamo2@studio.unibo.it>
2023-07-05 12:09:34 +0200

    fix power up structure

--- src/main/scala/model/powerUp/PowerUp.scala
+++ src/main/scala/model/powerUp/PowerUp.scala
@@ -2,26 +2,11 @@ package model.powerUp
 
 
 trait PowerUp:
-  def typePowerUp: PowerUpType
-  def getPrice: Int
+  def powerUpSetting: PowerUpSettings
   var hasBeenBought: Boolean = false
 
 object PowerUp:
-  def apply(typePowerUp: PowerUpType): PowerUp =
-    PowerUpImpl(typePowerUp)
+  def apply(powerUpSetting: PowerUpSettings): PowerUp =
+    PowerUpImpl(powerUpSetting)
 
-  private class PowerUpImpl(override val typePowerUp: PowerUpType) extends PowerUp:
-    import PowerUpType.*
-    override def getPrice: Int = typePowerUp match
-      case ColdResistanceI => 2
-      case ColdResistanceII => 2
-      case HotResistanceI => 2
-      case HotResistanceII => 2
-      case BacterialResistance => 2
-      case AirportEnablement => 2
-      case PortEnablement => 2
-      case InfectionThroughAnimals => 2
-      case InfectionThroughRespiratoryTract => 2
-      case MedicinesResistance => 2
-      case InfectedDrinkingWater => 2
-      case SpontaneousMutations => 2
+  private class PowerUpImpl(override val powerUpSetting: PowerUpSettings) extends PowerUp
