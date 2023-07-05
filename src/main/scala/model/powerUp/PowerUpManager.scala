1b012a3e447d04636d23d784a9d349e41a59104d
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-07-05 13:43:11 +0200

    Start create power up manager

--- src/main/scala/model/powerUp/PowerUpManager.scala
+++ src/main/scala/model/powerUp/PowerUpManager.scala
@@ -4,10 +4,9 @@ import model.powerUp.Filters.PowerUpFilter
 
 object Filters:
   type PowerUpFilter = PowerUp => Boolean
-  
+
   given PowerUpFilter = _ => true
 
 class PowerUpManager:
   private val powerUps: List[PowerUp] = PowerUpType.values.map(powerUpType => PowerUp(powerUpType)).toList
-  
   def getPowerUps(using filter: PowerUpFilter): List[PowerUp] = powerUps.filter(filter)
