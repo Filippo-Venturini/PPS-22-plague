9c4b663d92fb9a68655a59320179ca85a69f92c1
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-07-05 10:31:54 +0200

    Change bars graphic

--- src/main/scala/view/AllRegionsPanel.scala
+++ src/main/scala/view/AllRegionsPanel.scala
@@ -5,21 +5,29 @@ import model.world.Region
 import java.awt.{Color, Component}
 import javax.swing.{Box, BoxLayout, JComponent, JLabel, JPanel, JProgressBar, SwingConstants}
 import java.awt.Dimension
+import javax.swing.plaf.basic.BasicProgressBarUI
 
 class AllRegionsPanel(var regions: List[Region]) extends JPanel:
   var progressBars: Map[Region, JProgressBar] = Map()
-  this.setBackground(new Color(217,217,217))
+  val backgroundColor: Color = new Color(217,217,217)
+  val barBackgroundColor: Color = new Color(35, 187, 197)
+  val barForegroundColor: Color = new Color(215, 19, 19)
+  val barUI: BasicProgressBarUI = new BasicProgressBarUI() {
+    override def getSelectionBackground: Color = Color.white
+    override def getSelectionForeground: Color = Color.white
+  }
+  this.setBackground(backgroundColor)
   this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS))
-  val pb: JProgressBar = new JProgressBar
 
   regions.foreach(r => progressBars = progressBars + (r -> new JProgressBar()))
   progressBars.foreach((r, p) => {
     p.setMaximum(r.population)
-    //p.setBackground()
+    p.setBackground(barBackgroundColor)
+    p.setForeground(barForegroundColor)
     p.setStringPainted(true)
+    p.setUI(barUI)
     this.add(Box.createRigidArea(new Dimension(320, 30)))
     val regionNameLabel: JLabel = new JLabel(r.name, SwingConstants.CENTER)
-    regionNameLabel.setBackground(new Color(255,0,0))
     this.add(regionNameLabel)
     this.add(p)
   })
