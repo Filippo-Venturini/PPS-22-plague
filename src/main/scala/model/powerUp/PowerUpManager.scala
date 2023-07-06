38159ee9d30d48e56a2c56e9fe219d7663a3ac3f
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-07-06 13:46:31 +0200

    Refactor powerup manager

--- src/main/scala/model/powerUp/PowerUpManager.scala
+++ src/main/scala/model/powerUp/PowerUpManager.scala
@@ -1,20 +1,16 @@
 package model.powerUp
 
 import model.infection.Virus
-import model.powerUp.Filters.PowerUpFilter
 
-object Filters:
-  type PowerUpFilter = PowerUp => Boolean
-  given PowerUpFilter = _ => true
-  //val purchasablePowerUps: PowerUpFilter = powerUp => !powerUp.hasBeenBought && PowerUpManager.arePrerequisiteSatisfied(powerUp)
-  val purchasedPowerUps: PowerUpFilter = powerUp => powerUp.hasBeenBought
-
-class PowerUpManager(virus: Virus):
+class PowerUpManager(private val virus: Virus):
   private val powerUps: List[PowerUp] = PowerUpType.values.map(powerUpType => PowerUp(powerUpType)).toList
-  def getPowerUps(using filter: PowerUpFilter): List[PowerUp] = powerUps.filter(filter)
-  def getPowerUp(powerUpType: PowerUpType): Option[PowerUp] = powerUps.find(powerUp => powerUp.powerUpType == powerUpType)
+  def getAllPowerUps(): List[PowerUp] = this.powerUps
+  def getPurchasablePowerUps(): List[PowerUp] = this.powerUps.filter(powerUp => !powerUp.hasBeenBought && this.arePrerequisiteSatisfied(powerUp))
+  def getPurchasedPowerUps(): List[PowerUp] = this.powerUps.filter(powerUp => powerUp.hasBeenBought)
+  def getPowerUp(powerUpType: PowerUpType): Option[PowerUp] = this.powerUps.find(powerUp => powerUp.powerUpType == powerUpType)
   def arePrerequisiteSatisfied(powerUp: PowerUp): Boolean =
     powerUp.powerUpType.prerequisite.isEmpty ||
       powerUp.powerUpType.prerequisite.forall(prerequisite => powerUps.find(_.powerUpType == prerequisite).get.hasBeenBought)
-  def buyPowerUp(powerUpType: PowerUpType): Unit =
+  def purchasePowerUp(powerUpType: PowerUpType): Unit =
     this.powerUps.find(powerUp => powerUp.powerUpType == powerUpType).get.hasBeenBought = true
+
--- src/test/scala/model/powerUp/TestPowerUpManager.scala
+++ src/test/scala/model/powerUp/TestPowerUpManager.scala
@@ -3,7 +3,6 @@ package model.powerUp
 import model.infection.{BasicVirus, VirusConfiguration}
 import org.junit.{Before, Test}
 import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
-import model.powerUp.Filters.{purchasedPowerUps, given}
 
 class TestPowerUpManager {
   val testVirusConfiguration: VirusConfiguration = VirusConfiguration("DHT11", 0, 0, 0, 0, 0, 0, 0, false, false)
@@ -16,27 +15,27 @@ class TestPowerUpManager {
   @Test
   def testGetAllPowerUps(): Unit =
     val numberOfPowerUps = PowerUpType.values.map(powerUpType => PowerUp(powerUpType)).length
-    assertEquals(numberOfPowerUps, powerUpManager.getPowerUps.length)
+    assertEquals(numberOfPowerUps, powerUpManager.getAllPowerUps().length)
 
   @Test
   def testGetPowerUp(): Unit =
     assertEquals(PowerUpType.HotResistanceI, powerUpManager.getPowerUp(PowerUpType.HotResistanceI).get.powerUpType)
 
- /* @Test
+  @Test
   def testGetPurchasablePowerUpsWithoutPrerequisites(): Unit =
-    assertEquals(PowerUpType.values.filter(p => p.prerequisite.isEmpty).toList, powerUpManager.getPowerUps(using purchasablePowerUps).map(p => p.powerUpType))*/
+    assertEquals(PowerUpType.values.filter(p => p.prerequisite.isEmpty).toList, powerUpManager.getPurchasablePowerUps().map(p => p.powerUpType))
 
   @Test
   def testPowerUpBuy: Unit =
-    powerUpManager.buyPowerUp(PowerUpType.ColdResistanceI)
+    powerUpManager.purchasePowerUp(PowerUpType.ColdResistanceI)
     assertTrue(powerUpManager.getPowerUp(PowerUpType.ColdResistanceI).get.hasBeenBought)
 
   @Test
   def testPurchasedPowerUps: Unit =
-    powerUpManager.buyPowerUp(PowerUpType.ColdResistanceI)
-    powerUpManager.buyPowerUp(PowerUpType.AirportEnablement)
-    powerUpManager.buyPowerUp(PowerUpType.InfectionThroughAnimals)
-    assertEquals(3, powerUpManager.getPowerUps(using purchasedPowerUps).size)
+    powerUpManager.purchasePowerUp(PowerUpType.ColdResistanceI)
+    powerUpManager.purchasePowerUp(PowerUpType.AirportEnablement)
+    powerUpManager.purchasePowerUp(PowerUpType.InfectionThroughAnimals)
+    assertEquals(3, powerUpManager.getPurchasedPowerUps().size)
 
 
 }
