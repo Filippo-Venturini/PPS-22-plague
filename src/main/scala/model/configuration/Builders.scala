06fc45d9d07497b287d427dbed0a891ab630d76c
Nicolò Malucelli <nicolo.malucelli@studio.unibo.it>
2023-07-06 11:23:13 +0200

    regionIdentifier identifier regex

--- src/main/scala/model/configuration/Builders.scala
+++ src/main/scala/model/configuration/Builders.scala
@@ -138,8 +138,9 @@ object Builders:
   case class RegionIdentifier(regionName: String, identifier: String)
 
   case class RegionIdentifierBuilder(regionName: Option[String], identifier: Option[String]):
+    private def isIdValid(str: String): Boolean = str.toUpperCase.matches("#[0-9A-F]{6}")
     def setRegionName(name: Name): RegionIdentifierBuilder = RegionIdentifierBuilder(Some(name), identifier)
-    def setIdentifier(identifier: String): RegionIdentifierBuilder = RegionIdentifierBuilder(regionName, Some(identifier))
+    def setIdentifier(identifier: String): RegionIdentifierBuilder = if isIdValid(identifier) then RegionIdentifierBuilder(regionName, Some(identifier)) else this
     def build(): Option[RegionIdentifier] = this match
       case RegionIdentifierBuilder(Some(name), Some(identifier)) => Some(RegionIdentifier(name, identifier))
       case _ => None
--- src/test/scala/model/configuration/TestRegionIdentifierBuilder.scala
+++ src/test/scala/model/configuration/TestRegionIdentifierBuilder.scala
@@ -8,7 +8,7 @@ import org.junit.{Before, Test}
 
 class TestRegionIdentifierBuilder {
 
-  private val configuration: RegionIdentifier = RegionIdentifier("Central-Europe", "#AE12ED")
+  private val configuration: RegionIdentifier = RegionIdentifier("Central-Europe", "#AE12EF")
   private var builder: RegionIdentifierBuilder = RegionIdentifierBuilder()
 
   @Before
@@ -24,6 +24,36 @@ class TestRegionIdentifierBuilder {
   @Test
   def testIdentifierIsUnsetByDefault(): Unit = {
     assertEquals(None, builder.identifier)
+  }
+
+  @Test
+  def testRegionNameSet(): Unit = {
+    assertEquals(Some(configuration.regionName), builder.setRegionName(configuration.regionName).regionName)
+  }
+
+  @Test
+  def testIdentifierSet(): Unit = {
+    assertEquals(Some(configuration.identifier), builder.setIdentifier(configuration.identifier).identifier)
+  }
+
+  @Test
+  def testMalformedIdentifiersNotStartingWithHash(): Unit = {
+    assertEquals(None, builder.setIdentifier("ABCDEF").identifier)
+  }
+
+  @Test
+  def testMalformedIdentifiersTooLong(): Unit = {
+    assertEquals(None, builder.setIdentifier("#1234567").identifier)
+  }
+
+  @Test
+  def testMalformedIdentifiersTooShort(): Unit = {
+    assertEquals(None, builder.setIdentifier("#12345").identifier)
+  }
+
+  @Test
+  def testMalformedIdentifiersNotValidChar(): Unit = {
+    assertEquals(None, builder.setIdentifier("#0000Z0").identifier)
   }
 
   @Test
