d36fe99e44fdb0c52160f7f354f90206e7c79e5f
Nicolò Malucelli <nicolo.malucelli@studio.unibo.it>
2023-07-10 10:20:00 +0200

    dnaPoint refactor

--- src/main/scala/model/dnapoints/DnaPoints.scala
+++ src/main/scala/model/dnapoints/DnaPoints.scala
@@ -2,6 +2,9 @@ package model.dnapoints
 
 object DnaPoints {
 
+  trait DnaPoint:
+    def collect(): Unit
+
   trait DnaPointsHandler:
     def collectedPoints: Int
 
@@ -10,14 +13,21 @@ object DnaPoints {
     def spawnDnaPoint(): DnaPoint
 
   object DnaPointsHandler:
-    def apply(): DnaPointsHandler = new SimpleDnaPointsHandler(0)
+    def apply(): DnaPointsHandler = new BasicDnaPointsHandler(0)
 
-    private class SimpleDnaPointsHandler(override var collectedPoints: Int) extends DnaPointsHandler :
-      override def spawnDnaPoint(): DnaPoint = DnaPoint(this)
+    private class BasicDnaPointsHandler(override var collectedPoints: Int) extends DnaPointsHandler :
+      private var spawnedPoints: List[DnaPoint] = List()
 
-  case class DnaPoint(handler: DnaPointsHandler):
-    private var collected = false
-    def collect(): Unit = this.collected match
-      case false => {this.collected = true; handler.collectedPoints = handler.collectedPoints + 1}
-      case _ => 
+      override def spawnDnaPoint(): DnaPoint =
+        spawnedPoints = BasicDnaPoint(this) +: spawnedPoints
+        spawnedPoints.head
+
+      case class BasicDnaPoint(handler: BasicDnaPointsHandler) extends DnaPoint:
+        private var collected = false
+        def collect(): Unit = this.collected match
+          case false =>
+            this.collected = true
+            handler.collectedPoints = handler.collectedPoints + 1
+            handler.spawnedPoints = handler.spawnedPoints.filterNot(_ == this)
+          case _ =>
 }
