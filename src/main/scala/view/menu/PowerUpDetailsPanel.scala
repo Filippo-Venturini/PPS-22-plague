ec2c7d72b537f40f5eeacb538c14689cc072c7b0
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-07-11 11:41:26 +0200

    Make panels refreshables

--- src/main/scala/view/menu/PowerUpDetailsPanel.scala
+++ src/main/scala/view/menu/PowerUpDetailsPanel.scala
@@ -1,5 +1,7 @@
 package view.menu
 
+import model.powerUp.PowerUp
+
 import java.awt.{BorderLayout, Color, GridLayout}
 import javax.swing.border.EmptyBorder
 import javax.swing.{BoxLayout, JButton, JLabel, JPanel}
@@ -8,8 +10,18 @@ class PowerUpDetailsPanel extends JPanel:
   this.setBackground(new Color(217,217,217))
   this.setBorder(new EmptyBorder(20,300,20,10))
   this.setLayout(new BorderLayout())
-  this.add(new JLabel("POWER UP NAME"), BorderLayout.NORTH)
-  this.add(new JLabel("+10 cold regions infectivity"), BorderLayout.CENTER)
-  this.add(new JLabel("Increase the resistance of the virus in regions with cold climate."), BorderLayout.SOUTH)
-  this.add(new RoundButton("B1"), BorderLayout.EAST)
+  val powerUpNameLabel: JLabel = new JLabel("POWER UP NAME")
+  val powerUpPriceLabel: JLabel = new JLabel("DNA Points Required: 5")
+  val powerUpEffectLabel: JLabel = new JLabel("+10 cold regions infectivity")
+  val powerUpDescriptionLabel: JLabel = new JLabel("Increase the resistance of the virus in regions with cold climate.")
+  this.add(powerUpNameLabel, BorderLayout.NORTH)
+  this.add(powerUpPriceLabel, BorderLayout.WEST)
+  this.add(powerUpEffectLabel, BorderLayout.CENTER)
+  this.add(powerUpDescriptionLabel, BorderLayout.SOUTH)
+  this.add(new RoundButton(), BorderLayout.EAST)
+
+  def refreshPowerUpInformations(powerUp: PowerUp): Unit =
+    this.powerUpNameLabel.setText(powerUp.powerUpType.toString)
+    this.powerUpPriceLabel.setText(powerUp.powerUpType.price.toString)
+
 
