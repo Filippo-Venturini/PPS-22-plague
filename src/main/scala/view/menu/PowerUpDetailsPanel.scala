38ce7b6aa027feebd2d58ee10ba612af7f650f98
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-07-13 10:44:57 +0200

    Refactor power up panel

--- src/main/scala/view/menu/PowerUpDetailsPanel.scala
+++ src/main/scala/view/menu/PowerUpDetailsPanel.scala
@@ -3,7 +3,7 @@ package view.menu
 import controller.MenuController
 import model.powerUp.PowerUp
 
-import java.awt.{BorderLayout, Color, Component, GridBagConstraints, GridBagLayout, GridLayout, Image}
+import java.awt.{BorderLayout, Color, Component, GridBagConstraints, GridBagLayout, GridLayout, Image, Insets}
 import javax.imageio.ImageIO
 import javax.swing.border.EmptyBorder
 import javax.swing.{BoxLayout, ImageIcon, JButton, JComponent, JLabel, JPanel}
@@ -15,11 +15,11 @@ class PowerUpDetailsPanel(val virusPanel: VirusPanel, val menuController: MenuCo
   val constraint: GridBagConstraints = new GridBagConstraints()
   this.constraint.fill = GridBagConstraints.HORIZONTAL
   var powerUpShowed: PowerUp = null
-  val powerUpNameLabel: JLabel = new JLabel("POWER UP NAME")
-  val powerUpPriceLabel: JLabel = new JLabel("Price: 5")
-  val DNAPointsLabel: JLabel = new JLabel("DNA Points: " + menuController.getCollectedDNAPoints())
-  val powerUpEffectLabel: JLabel = new JLabel("+10 cold regions infectivity")
-  val powerUpDescriptionLabel: JLabel = new JLabel("Increase the resistance of the virus in regions with cold climate.")
+  val powerUpNameLabel: JLabel = new JLabel()
+  val powerUpPriceLabel: JLabel = new JLabel()
+  val DNAPointsLabel: JLabel = new JLabel("DNA Points collected: " + this.menuController.getCollectedDNAPoints())
+  val powerUpEffectLabel: JLabel = new JLabel()
+  val powerUpDescriptionLabel: JLabel = new JLabel()
   val buyImage: Image = ImageIO.read(getClass.getResource("/buyPowerUp.png"))
   val buyButton: JButton = new JButton()
   buyButton.setIcon(new ImageIcon(buyImage))
@@ -33,13 +33,15 @@ class PowerUpDetailsPanel(val virusPanel: VirusPanel, val menuController: MenuCo
   this.constraint.gridx = 0
   this.constraint.gridy = 1
   this.constraint.gridwidth = 1
+  this.constraint.insets = new Insets(10,0,0,0)
   this.add(powerUpPriceLabel, this.constraint)
   this.constraint.gridx = 1
   this.constraint.gridy = 1
-  this.constraint.anchor = GridBagConstraints.PAGE_END
+  this.constraint.insets = new Insets(10,200,0,0)
   this.add(DNAPointsLabel, this.constraint)
   this.constraint.gridx = 0
   this.constraint.gridy = 2
+  this.constraint.insets = new Insets(10,0,0,0)
   this.add(powerUpEffectLabel, this.constraint)
   this.constraint.gridx = 0
   this.constraint.gridy = 3
@@ -51,7 +53,7 @@ class PowerUpDetailsPanel(val virusPanel: VirusPanel, val menuController: MenuCo
 
   this.buyButton.addActionListener(_ => {
     this.menuController.purchasePowerUp(this.powerUpShowed.powerUpType)
-    this.DNAPointsLabel.setText("DNA Points: " + this.menuController.getCollectedDNAPoints())
+    this.DNAPointsLabel.setText("DNA Points collected: " + this.menuController.getCollectedDNAPoints())
     this.buyButton.setEnabled(false)
     this.virusPanel.refreshVirusCharacteristics(this.menuController.getVirusConfiguration())
   })
