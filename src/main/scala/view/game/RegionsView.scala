63adf0d6b94c5e9c6d6c01035a08086e94833d40
Nicolò Malucelli <nicolo.malucelli@studio.unibo.it>
2023-07-14 11:55:52 +0200

    list borders in detail regions panel

--- src/main/scala/view/game/RegionsView.scala
+++ src/main/scala/view/game/RegionsView.scala
@@ -65,14 +65,8 @@ object RegionsView:
   case class SingleRegionPanel(region: Region) extends RefreshablePanel:
     this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS))
 
-    val infectedAmountLabel: JLabel = new JLabel(region.infectedAmount.toString)
-    val infectionBar: JProgressBar = new DecimalProgressBar(new Color(35, 187, 197), new Color(215, 19, 19))
-    infectionBar.setMaximum(region.population)
-
     val lblName = new JLabel(region.name)
     lblName.setFont(new Font("Arial", Font.PLAIN, 30))
-
-    this.addCenter(lblName)
 
     val statisticsPanel: JPanel = new JPanel(new GridLayout(3, 2, 20, 15))
     statisticsPanel.add(new JLabel("population: " + String.format("%,d", region.population)))
@@ -82,16 +76,38 @@ object RegionsView:
     statisticsPanel.add(new JLabel("borders control: " + region.bordersControl + "/5"))
     statisticsPanel.add(new JLabel("globalization: " + region.globalization + "/5"))
 
-    this.addCenter(statisticsPanel)
-    //this.add(new JLabel("climate: " + region.climate))
+    val infectedAmountLabel: JLabel = new JLabel(String.format("population infected: %,d", region.infectedAmount))
 
-    private def addCenter(comp: JComponent): Unit =
-      val container: JPanel = new JPanel()
-      container.add(comp)
-      this.add(container)
+    val infectionBar: JProgressBar = new DecimalProgressBar(new Color(35, 187, 197), new Color(215, 19, 19))
+    infectionBar.setMaximum(region.population)
 
-    this.addCenter(infectedAmountLabel)
-    this.add(infectionBar)
+    this.add(lblName, true)
+      .addPadding(20)
+      .add(statisticsPanel, false)
+      .addPadding(20)
+      .add(infectedAmountLabel, true)
+      .addPadding(10)
+      .add(infectionBar, false)
+
+    region.getReachableRegions.groupMap((_, mode) => mode)((region, _) => region)
+      .map(e => (e._1, e._2.map(r => r.name).foldLeft("")((n, sum) => sum + "\n" + n)))
+      .map((mode, regions) => String.format("Reachable by %s:\n %s", mode.toString.toLowerCase(), regions))
+      .map(mode => new JLabel(String.format("<html>%s", mode.toString.replace("\n", "<br>"))))
+      .foldLeft(this.addPadding(20))((panel, label) => panel.add(label, true).addPadding(10))
+
+    private def add(comp: JComponent, center: Boolean): SingleRegionPanel = center match
+      case true =>
+        val container: JPanel = new JPanel()
+        container.add(comp)
+        this.add(container)
+        this
+      case false =>
+        this.add(comp)
+        this
+
+    private def addPadding(padding: Int): SingleRegionPanel =
+      if padding > 0 then this.add(Box.createRigidArea(new Dimension(0, padding)))
+      this
 
     override def refresh(): Unit =
       infectionBar.setValue(region.infectedAmount)
