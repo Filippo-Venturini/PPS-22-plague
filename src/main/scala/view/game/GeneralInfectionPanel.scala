8fd2ad360fe0787756458047d29951df0492d8d1
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-07-14 11:54:51 +0200

    Add feature to general infection panel

--- src/main/scala/view/game/GeneralInfectionPanel.scala
+++ src/main/scala/view/game/GeneralInfectionPanel.scala
@@ -3,7 +3,7 @@ package view.game
 import controller.GameEngine
 import view.game.RegionsView.{DecimalProgressBar, RefreshablePanel}
 
-import java.awt.Color
+import java.awt.{Color, Font}
 import javax.swing.{BoxLayout, JLabel, JPanel}
 
 class GeneralInfectionPanel(val gameEngine: GameEngine) extends RefreshablePanel:
@@ -11,14 +11,16 @@ class GeneralInfectionPanel(val gameEngine: GameEngine) extends RefreshablePanel
   val worldInfectionProgressBar: DecimalProgressBar = new DecimalProgressBar(new Color(35, 187, 197), new Color(215, 19, 19))
   this.worldInfectionProgressBar.setMaximum((this.gameEngine.getWorldPopulation / 10).toInt)
   val infectedAmountLabel: JLabel = new JLabel()
-  val totalPopulationLabel: JLabel = new JLabel("World Population: " + this.gameEngine.getWorldPopulation.toString)
-  this.add(new JLabel("World"))
+  val totalPopulationLabel: JLabel = new JLabel("World Population: " + String.format("%,d", this.gameEngine.getWorldPopulation))
+  val worldTitleLabel: JLabel = new JLabel("World")
+  worldTitleLabel.setFont(new Font("Arial", Font.PLAIN, 30))
+  this.add(worldTitleLabel)
   this.add(totalPopulationLabel)
   this.add(infectedAmountLabel)
   this.add(worldInfectionProgressBar)
 
   override def refresh(): Unit =
-    this.infectedAmountLabel.setText("Infected Amount: " + this.gameEngine.getWorldInfectedAmount)
+    this.infectedAmountLabel.setText("Infected Amount: " + String.format("%,d",this.gameEngine.getWorldInfectedAmount))
     this.worldInfectionProgressBar.setValue((this.gameEngine.getWorldInfectedAmount / 10).toInt)
 
 
