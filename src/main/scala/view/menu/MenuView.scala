5570945fc9b185ab360178a33b4373952f275a89
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-07-19 08:50:08 +0200

    Add scaladoc to menu view

--- src/main/scala/view/menu/MenuView.scala
+++ src/main/scala/view/menu/MenuView.scala
@@ -5,21 +5,25 @@ import controller.{GameEngine, MenuController}
 import java.awt.{BorderLayout, Color, Dimension, Frame, Toolkit}
 import javax.swing.JFrame
 
+/**
+ * Class that represent the menu that contains: the power up' hierarchy, the virus information and the power up details
+ *
+ * @param menuController : the controller for handling the menu
+ */
 class MenuView (val menuController: MenuController):
-  val frame: JFrame = new JFrame()
-  frame.setBackground(new Color(255,255,255))
-  val virusPanel: VirusPanel = new VirusPanel(menuController.getVirusConfiguration)
-  val powerUpDetailsPanel: PowerUpDetailsPanel = new PowerUpDetailsPanel(virusPanel, menuController)
-  val powerUpsGridPanel: PowerUpsGridPanel = new PowerUpsGridPanel(powerUpDetailsPanel, menuController)
+  private val frame: JFrame = new JFrame()
+  private val virusPanel: VirusPanel = new VirusPanel(menuController.getVirusConfiguration)
+  private val powerUpDetailsPanel: PowerUpDetailsPanel = new PowerUpDetailsPanel(virusPanel, menuController)
+  private val powerUpsGridPanel: PowerUpsGridPanel = new PowerUpsGridPanel(powerUpDetailsPanel, menuController)
   
   powerUpDetailsPanel.setPowerUpsGridPanel(powerUpsGridPanel)
 
   frame.setTitle("Menu")
+  frame.setBackground(new Color(255,255,255))
   frame.add(virusPanel, BorderLayout.NORTH)
   frame.add(powerUpsGridPanel, BorderLayout.CENTER)
   frame.add(powerUpDetailsPanel, BorderLayout.SOUTH)
   frame.pack()
   frame.setSize(1200,800)
-  //frame.setExtendedState(Frame.MAXIMIZED_BOTH)
   frame.setResizable(false)
   frame.setVisible(true)
